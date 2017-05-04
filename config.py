9093d428da201ce71655b9da0b88c804f7aa6b48
shaolizhi1234 <674940850@qq.com>
2017-05-04 20:22:55 +0800

    配置选项

--- config.py
+++ config.py
@@ -1,2 +1,48 @@
 import os
-basedir = os.path.abspath()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# 基类Config中包含通用配置，子类分别定义专用的配置，为了让配置方式更灵活更安全，有些配置可以从环境变量中导入，
+# 并使用os.environ.get(String)函数导入，但是系统也提供了一个默认值，以防止环境中没有定义。
+class Config:
+    SECRET_KET = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[shaolizhi]'
+    FLASKY_MAIL_SENDER = 'shaolizhi <shaolizhi1234@126.com>'
+    FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_POST = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
+                                                                                                'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
+                                                                                                 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
+                                                                                            'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
